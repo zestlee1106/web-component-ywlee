13c37de03d71eafd26430aa1fbbf73caef2883c1
이영우 <zestlee1106@gmail.com>
2023-04-04 23:10:36 +0900

    main 파일에 vue 파일을 h 함수로 만들어 준 것을 export 하도록 추가

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 export { default as BaseButton } from "./src/components/base/button/button";
 export { default as MyElement } from "./src/my-element";
+
+import { h } from "vue";
+
+export const BaseButton2 = h("base-button");
